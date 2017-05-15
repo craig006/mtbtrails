465555f67a14b348e84281541046b1f09da780a1
Craig Thomas <Craig@Craigs-MacBook-Pro.local>
2017-05-15 19:32:38 +0200

    Deserialize trail option

--- Model/TrailOption.swift
+++ Model/TrailOption.swift
@@ -1,6 +1,7 @@
 import Foundation
+import SwiftyJSON
 
-struct TrailOption {
+struct TrailOption: Deserializable {
     var title: String = ""
     var length: Int = 0
     var climb: Int = 0
@@ -10,5 +11,19 @@ struct TrailOption {
         get {
             return Int(round(Double(length) / Double(1000)))
         }
+    }
+
+    init(title: String, length: Int, climb: Int, imageUrl: String) {
+        self.title = title
+        self.length = length
+        self.climb = climb
+        self.imageUrl = imageUrl
+    }
+
+    init(json: JSON) {
+        title = json["name"].string ?? ""
+        length = json["distance"].int ?? 0
+        climb = json["climb"].int ?? 0
+        imageUrl = json["iconurl"].string ?? ""
     }
 }
