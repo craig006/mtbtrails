2df2dd48cfc6de8db391b2b5c0b74945c8cb7c90
Craig Thomas <Craig@Craigs-MacBook-Pro.local>
2017-05-15 19:30:57 +0200

    Implement live trail fetch action

--- Action/LiveTrailFetchAction.swift
+++ Action/LiveTrailFetchAction.swift
@@ -8,7 +8,12 @@ class LiveTrailFetchAction: TrailFetchAction {
         self.trailService = trailService;
     }
 
-    func begin() -> Signal<Trail, AppError> {
-        return Signal<Trail, AppError> { _ in return BlockDisposable { } }
+    func begin(id: String) -> Signal<Trail, AppError> {
+        return Signal<Trail, AppError> { observer in
+            trailService.fetchTrail(id).observeNext { trail in
+                observer.completed(with: trail)
+            }
+            return BlockDisposable { }
+        }
     }
 }
