9ab443e327ee3f6dacdc612c6c2997b62cc8c388
Craig Thomas <Craig@Craigs-MacBook-Pro.local>
2017-05-15 19:31:13 +0200

    Implement local trail fetch action

--- Action/LocalTrailFetchAction.swift
+++ Action/LocalTrailFetchAction.swift
@@ -9,8 +9,14 @@ class LocalTrailFetchAction: TrailFetchAction {
         self.trail = trail
     }
 
-    func begin() -> Signal<Trail, AppError> {
+    func begin(id: String) -> Signal<Trail, AppError> {
         return Signal<Trail, AppError> { observer in
+            guard self.trail.id == id else {
+                var error = AppError(userMessage: "Oh no, unable to view this trail at this time, please try again later",
+                        technicalMessage: "Trail with id:\(id) not found in local cache",
+                        type: .CacheError)
+                observer.failed(error)
+            }
             observer.completed(with: self.trail)
             return BlockDisposable {}
         }
